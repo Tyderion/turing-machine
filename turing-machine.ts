96c5878808d11efe4788de0327a6f97896f325ca
Gabriel Nadler <nadlergabriel@gmail.com>
2017-04-25 18:14:43 +0200

    Accept command line arguments

--- turing-machine.ts
+++ turing-machine.ts
@@ -20,7 +20,11 @@ export class TuringMachine {
         return this.band.read();
     }
 
-    public constructor(private left: number, private right: number, private printSteps: boolean = false) {
+    private get printSteps(): boolean {
+        return this.delay > 0;
+    }
+
+    public constructor(private left: number, private right: number, private delay: number = 0) {
         let input = '';
         for (let i = 0; i < this.left; i++) {
             input += DOT;
@@ -29,11 +33,11 @@ export class TuringMachine {
         for (let i = 0; i < this.right; i++) {
             input += DOT
         }
-        this.band = new Band(input.split(''), 30, printSteps);
+        this.band = new Band(input.split(''), 30, this.printSteps);
     }
 
     public compute(): void {
-        this.step(this.printSteps ? 100 : 0).then(() => this.compute()).catch(() => {
+        this.step(this.delay).then(() => this.compute()).catch(() => {
             if (!this.printSteps) {
                 this.band.printBand();
                 console.log('')
@@ -41,32 +45,25 @@ export class TuringMachine {
             console.log(`\nComputed ${this.left} * ${this.right} in ${this.band.count} steps`);
             console.log('Final State: ', this.current);
             console.log(`Result: `, this.band.number);
-        })
-        // while (this.current !== -1) {
-        //     try {
-        //         this.step();
-        //     } catch (err) {
-        //         break;
-        //     }
-        // }
-
+        });
     }
 
     private step(delay: number = 0): Promise<any> {
-        // if (delay === 0) {
-        //     this.doStep();
-        // } else {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                try {
-                    this.doStep();
-                    resolve();
-                } catch (err) {
-                    reject(err);
-                }
-            }, delay);
-        });
-        // }
+        if (delay === 0) {
+            this.doStep();
+            return Promise.resolve();
+        } else {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        this.doStep();
+                        resolve();
+                    } catch (err) {
+                        reject(err);
+                    }
+                }, delay);
+            });
+        }
     }
 
     private doStep() {
